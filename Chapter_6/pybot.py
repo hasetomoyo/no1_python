662764ae94780091f7c9b8c0653fc2c12e18e59b
GMOPG\E024779 <hasegawa@gmo-pg.com>
2017-08-24 16:15:21 +0900

    Lesson35終了

--- Chapter_6/pybot.py
+++ Chapter_6/pybot.py
@@ -1,16 +1,21 @@
-bot_dict = {
-    'こんにちは': 'コンニチハ',
-    'ありがとう': 'ドウイタシマシテ',
-    'さようなら': 'サヨウナラ',
-    'かわいいね': 'アリガトウ',
-    }
+command_file = open('pybot.txt', encoding='utf-8')
+raw_data = command_file.read()
+command_file.close()
+lines = raw_data.splitlines()
+
+bot_dict = {}
+for line in lines:
+    word_list = line.split(',')
+    key = word_list[0]
+    response = word_list[1]
+    bot_dict[key] = response
 
 while True:
     command = input('pybot> ')
     response = ''
-    for message in bot_dict:
-        if message in command:
-            response = bot_dict[message]
+    for key in bot_dict:
+        if key in command:
+            response = bot_dict[key]
             break
 
     if not response:

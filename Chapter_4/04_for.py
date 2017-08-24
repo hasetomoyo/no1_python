6339ea40d473e9c00c59bd31d667b91559038ed1
GMOPG\E024779 <hasegawa@gmo-pg.com>
2017-08-24 12:10:52 +0900

    no message

--- Chapter_4/04_for.py
+++ Chapter_4/04_for.py
@@ -1,8 +1,12 @@
 point_list = [75, 80, 91]
 total = 0
-for print in point_list:
-    total += print
+for point in point_list:
+    total += point
 
 number_of_subjects = len(point_list)
 average = total / number_of_subjects
 print('合計点は{}、平均点は{}です。'.format(total, average))
+
+# P.101記載情報[enumerate]
+for index, color in enumerate(['red', 'blue', 'green']):
+    print("No.{} is {}".format(index, color))

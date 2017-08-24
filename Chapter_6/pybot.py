2cb65e83733db29b562ba55d9bfdf68386fd739f
GMOPG\E024779 <hasegawa@gmo-pg.com>
2017-08-24 17:20:12 +0900

    修正

--- Chapter_6/pybot.py
+++ Chapter_6/pybot.py
@@ -1,5 +1,3 @@
-from pybot_eto import eto_command
-
 def len_command(command):
     cmd, text = command.split()
     length = len(text)
@@ -41,9 +39,6 @@ while True:
 
     if '長さ' in command:
         response = len_command(command)
-
-    if '干支' in command:
-        response = eto_command(command)
 
     if not response:
         response = '何ヲ言ッテイルカ、ワカラナイ'

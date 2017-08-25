13df52e620f6dddc0467c105d874ed17e92336d1
GMOPG\E024779 <hasegawa@gmo-pg.com>
2017-08-25 15:54:12 +0900

    Lesson52終了 wikipediaパッケージにプロキシ設定できる何かが見つからなかったので、試せてないけど一応完了

--- Chapter_8/requests_sample.py
+++ Chapter_8/requests_sample.py
@@ -1,7 +1,7 @@
 import requests
 
 city_code ='130010'
-r = requests.get('http://weather.livedoor.com/forecast/webservice/json/v1?city={}', proxies={"http" : 'http://e024779:tiyoyo7!@172.17.96.21:12080'})
+r = requests.get('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010', proxies={"http" : 'http://e024779:tiyoyo7!@172.17.96.21:12080'})
 weather_data = r.json()
 city = weather_data['location']['city']
 label = weather_data['forecasts'][0]['dateLabel']

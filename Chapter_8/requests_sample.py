123dbe91b05209e166de1a7339206835cae753dd
GMOPG\E024779 <hasegawa@gmo-pg.com>
2017-08-25 14:37:14 +0900

    Lesson50終了

--- Chapter_8/requests_sample.py
+++ Chapter_8/requests_sample.py
@@ -1,5 +1,5 @@
 import requests
 
-r = requests.get('http://book.impress.co.jp/ --proxy=http://e024779:tiyoyo7!@172.17.96.21:12080')
+r = requests.get('http://book.impress.co.jp/', proxies={"http" : 'http://e024779:tiyoyo7!@172.17.96.21:12080'})
 
 print(r.status_code)
